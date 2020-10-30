b349ff3529e5b867fd833018afd719f50da28f62
Charles Korn <me@charleskorn.com>
2020-10-30 15:18:27 +1100

    Fix outstanding formatting issue.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,56 +14,31 @@ interface FeatureDetails {
 const features: FeatureDetails[] = [
   {
     title: "Fast",
-    description: (
-      <>
-        Tasks start quickly due to parallelisation and run quickly thanks to
-        caching. We&rsquo;ve seen 17% quicker execution than Docker Compose.
-      </>
-    ),
+    description: <>Tasks start quickly due to parallelisation and run quickly thanks to caching. We&rsquo;ve seen 17% quicker execution than Docker Compose.</>,
   },
   {
     title: "Consistent",
-    description: (
-      <>
-        Batect uses Docker to create a clean, isolated environment every time
-        you run a task. No more &ldquo;works on my machine&rdquo;.
-      </>
-    ),
+    description: <>Batect uses Docker to create a clean, isolated environment every time you run a task. No more &ldquo;works on my machine&rdquo;.</>,
   },
   {
     title: "Everywhere",
     description: (
-      <>
-        Your machine, your colleagues&rsquo; machines and CI: everyone runs
-        exactly the same thing, every time. Works on macOS, Linux and even
-        Windows.
-      </>
+      <>Your machine, your colleagues&rsquo; machines and CI: everyone runs exactly the same thing, every time. Works on macOS, Linux and even Windows.</>
     ),
   },
   {
     title: "Reusable",
-    description: (
-      <>
-        Share tasks between projects with bundles. And use existing bundles to
-        get up and running quickly.
-      </>
-    ),
+    description: <>Share tasks between projects with bundles. And use existing bundles to get up and running quickly.</>,
   },
   {
     title: "Automated",
-    description: (
-      <>
-        Free your team from manual setup and maintenance of tools and
-        dependencies: everyone automatically uses the same configuration.
-      </>
-    ),
+    description: <>Free your team from manual setup and maintenance of tools and dependencies: everyone automatically uses the same configuration.</>,
   },
   {
     title: "Simple",
     description: (
       <>
-        No installation required, just drop the script in your project and run{" "}
-        <code>./batect</code>.
+        No installation required, just drop the script in your project and run <code>./batect</code>.
       </>
     ),
   },
@@ -87,17 +62,10 @@ function Home() {
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">
-            The <strong>fast</strong>, <strong>consistent</strong> way to run
-            your development and testing tasks <strong>everywhere</strong>.
+            The <strong>fast</strong>, <strong>consistent</strong> way to run your development and testing tasks <strong>everywhere</strong>.
           </p>
           <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                "button button--secondary button--lg",
-                styles.getStarted
-              )}
-              to={useBaseUrl("docs/")}
-            >
+            <Link className={clsx("button button--secondary button--lg", styles.getStarted)} to={useBaseUrl("docs/")}>
               Get Started
             </Link>
           </div>
