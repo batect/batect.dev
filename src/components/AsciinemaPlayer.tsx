edf78096bdf43e4157bfdd0b705a5ccd856fbcca
Charles Korn <me@charleskorn.com>
2020-11-05 10:39:59 +1100

    Extract useScript hook and remove debugging output.

--- src/components/AsciinemaPlayer.tsx
+++ src/components/AsciinemaPlayer.tsx
@@ -1,5 +1,6 @@
-import React, { Component, Ref, useEffect, useState } from "react";
+import React, { Component, Ref } from "react";
 import paths from "../plugins/asciinema-player/paths";
+import useScript from "../utils/useScript";
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace asciinema {
@@ -48,63 +49,19 @@ class AsciinemaPlayer extends Component<AsciinemaPlayerProps> {
   }
 }
 
-// From https://usehooks.com/useScript/
-function useScript(src) {
-  const [status, setStatus] = useState(src ? "loading" : "idle");
-
-  useEffect(() => {
-    if (!src) {
-      setStatus("idle");
-      return;
-    }
-
-    let script = document.querySelector<HTMLScriptElement>(`script[src="${src}"]`);
-
-    if (!script) {
-      script = document.createElement("script");
-      script.src = src;
-      script.async = true;
-      script.setAttribute("data-status", "loading");
-      document.body.appendChild(script);
-
-      const setAttributeFromEvent = (event) => {
-        script.setAttribute("data-status", event.type === "load" ? "ready" : "error");
-      };
-
-      script.addEventListener("load", setAttributeFromEvent);
-      script.addEventListener("error", setAttributeFromEvent);
-    } else {
-      setStatus(script.getAttribute("data-status"));
-    }
-
-    const setStateFromEvent = (event) => {
-      setStatus(event.type === "load" ? "ready" : "error");
-    };
-
-    script.addEventListener("load", setStateFromEvent);
-    script.addEventListener("error", setStateFromEvent);
-
-    return () => {
-      if (script) {
-        script.removeEventListener("load", setStateFromEvent);
-        script.removeEventListener("error", setStateFromEvent);
-      }
-    };
-  }, [src]);
-
-  return status;
-}
-
 const AsciinemaPlayerWrapper = (props: AsciinemaPlayerProps) => {
   const status = useScript(paths.js);
-  console.log(status);
 
-  if (status !== "ready") {
-    return <div>Loading...</div>;
+  switch (status) {
+    case "ready":
+      return <AsciinemaPlayer {...props} />;
+    case "loading":
+      return <div>Loading...</div>;
+    case "error":
+      return <div>Loading player failed.</div>;
+    default:
+      throw new Error(`Unknown script status: ${status}`);
   }
-
-  console.log("Showing");
-  return <AsciinemaPlayer {...props} />;
 };
 
 export default AsciinemaPlayerWrapper;
