43e23d88e612ffee251f43e572b3847e1002aba1
Charles Korn <me@charleskorn.com>
2021-07-17 09:26:57 +1000

    Switch Remark configuration to ESM.

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,4 +1,16 @@
-module.exports = {
+import { unified } from "unified";
+
+import remark_retext from "remark-retext";
+import retext_english from "retext-english";
+import retext_syntax_mentions from "retext-syntax-mentions";
+import retext_syntax_urls from "retext-syntax-urls";
+import retext_diacritics from "retext-diacritics";
+import retext_indefinite_article from "retext-indefinite-article";
+import retext_redundant_acronyms from "retext-redundant-acronyms";
+import retext_repeated_words from "retext-repeated-words";
+import retext_sentence_spacing from "retext-sentence-spacing";
+
+export default {
   plugins: [
     // https://github.com/remarkjs/remark-lint#list-of-presets
     "remark-preset-lint-consistent",
@@ -14,16 +26,16 @@ module.exports = {
     "remark-validate-links",
 
     [
-      require("remark-retext"),
-      require("unified")()
-        .use(require("retext-english"))
-        .use(require("retext-syntax-mentions"))
-        .use(require("retext-syntax-urls"))
-        .use(require("retext-diacritics"))
-        .use(require("retext-indefinite-article"))
-        .use(require("retext-redundant-acronyms"))
-        .use(require("retext-repeated-words"))
-        .use(require("retext-sentence-spacing")),
+      remark_retext,
+      unified()
+        .use(retext_english)
+        .use(retext_syntax_mentions)
+        .use(retext_syntax_urls)
+        .use(retext_diacritics)
+        .use(retext_indefinite_article)
+        .use(retext_redundant_acronyms)
+        .use(retext_repeated_words)
+        .use(retext_sentence_spacing),
     ],
 
     // Rules enabled by default by presents above that we don't want
